2ca88de09333803dac9e23f5ec2b79447da0045e
yu-gotcha <yu.gotcha@gmail.com>
2021-08-04 14:15:18 +0900

    위클리 챌린지 1주차: 부족한 금액 계산하기

---
+++ src/programmers/weeklyChallenge/week1/CalculatingTheShortfall.java
@@ -0,0 +1,13 @@
+package programmers.weeklyChallenge.week1;
+
+
+/* 부족한 금액 계산하기 */
+public class CalculatingTheShortfall {
+    public static void main(String[] args) {
+        int price = 3, money = 20, count = 4;
+
+        long answer = price * ((count * (count + 1))/2) - money;
+        System.out.println(answer > 0 ? answer : 0);
+    }
+
+}
